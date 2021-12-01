8e314f8f48add33401516e30c7a2a52e3ac36fad
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-01 10:26:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
 
+bool funzione (int a, int b){
+    bool esito;
+    if(b % a != 0 && b>a) {
+        funzione(a + 1, b);
+    } else if (b % a == 0 && b > a ){
+        return esito =  false ;
+    } else {
+        return esito = true ;
+    }
+}
