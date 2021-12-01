296412288283f0abb8dde7f2b1badf8757bde2cf
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-01 10:26:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+    int b=0;
+    cin >> b;
+    int a=2;
+    if (funzione (a, b ) == true){
+        cout << "numero primo";
+    } else if (funzione (a, b)== false){
+        cout << "numero non primo";
+    }
+    return 0;
 }
